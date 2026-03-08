fb73b5d738ba7e0eedb60f3639e8dfdfba54eb27
emestestudio <emestestudio@gmail.com>
2026-03-07 21:37:33 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+function comprar(producto){
 
+let telefono="56900000000"
+
+let mensaje="Hola quiero comprar "+producto
+
+let url="https://wa.me/"+telefono+"?text="+encodeURIComponent(mensaje)
+
+window.open(url)
+
+}
